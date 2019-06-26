ac77dd874e682a4e9b13146da0c749b666cfbae7
AnikinKostya <52211374+AnikinKostya@users.noreply.github.com>
2019-06-26 22:38:17 +0400

    Add files via upload

---
+++ FindNumber/FindNumber/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace FindNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = new int [10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(10);                 
+                Console.WriteLine("Число массива: " + arr[i]);
+            }
+
+            int sort;
+
+            Console.WriteLine("Отсортированный массив: ");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        sort = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = sort;
+                    }
+                }
+
+                 Console.WriteLine("Число массива: " + arr[i]);
+
+            }
+
+            Console.Write("Введите число, которое нужно найти ");
+
+            int findNum = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i ++)
+            {
+                if (findNum == arr[i])
+                {
+                    Console.WriteLine("Заданное число найдено ");
+                }
+                else
+                {
+                   Console.WriteLine("Заданное число не найдено ");
+                }
+                
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
